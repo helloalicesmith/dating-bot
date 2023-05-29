98a0ee7125b034da8530c26a845c4fd64319bbac
helloalicesmith <helloalicesmith@gmail.com>
2023-05-29 16:25:25 +0300

    fix

--- src/commands/profile/index.js
+++ src/commands/profile/index.js
@@ -1,7 +1,6 @@
 const Composer = require('../../composer.js')
 const { composer: menu, profileMenu } = require('./menu/index.js')
 const conversation = require('./conversation/index.js')
-const { userKeyboard } = require('../../common/keyboards.js')
 const api = require('../../api/api.js')
 
 const composer = new Composer()
@@ -31,31 +30,9 @@ const profileCommand = async (ctx) => {
     await ctx.reply(profile, { reply_markup: profileMenu, parse_mode: 'HTML' })
 }
 
-const getLocation = async (ctx) => {
-    const { id } = ctx.message.from
-    const { latitude: lat, longitude: long } = ctx.message.location
-
-    const { data } = await api.geoService.getLocationByLatLong({
-        lat,
-        long,
-    })
-
-    // TODO save city by user local
-    const city = data[0].local_names.ru
-
-    await api.usersService.updateUser(id, {
-        city,
-    })
-
-    return await ctx.reply(ctx.t('profile.location_success'), {
-        reply_markup: userKeyboard,
-    })
-}
-
 composer.use(conversation)
 composer.use(menu)
 
 composer.command(['profile'], profileCommand)
-composer.on('msg:location', getLocation)
 
 module.exports = composer
